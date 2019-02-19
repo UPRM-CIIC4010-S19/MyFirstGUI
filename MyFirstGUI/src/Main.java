bf69afe48c0cd19787b883dfb5cca0c4f914018a
Bienvenido Velez <Bienvenido.Velez@upr.edu>
2019-02-19 17:40:45 -0400

    Added infinite repaint

--- MyFirstGUI/src/Main.java
+++ MyFirstGUI/src/Main.java
@@ -2,13 +2,20 @@ import javax.swing.JFrame;
 
 public class Main {
 
-		public static void main(String[] args) {
+		public static void main(String[] args) throws InterruptedException {
 			
 			JFrame mainFrame = new JFrame("My First GUI");
 			mainFrame.setSize(800, 600);
 			MyComponent myComponent = new MyComponent();
 			mainFrame.add(myComponent);
 			mainFrame.setVisible(true);
+			
+			while(true) {
+				
+				mainFrame.repaint();
+				Thread.sleep(100);
+				
+			}
 			
 		}
 	
